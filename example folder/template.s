8c91b7d7ffcdd680037a92145b2de7d0dcb5da94
Masonman101 <165975990+Masonman101@users.noreply.github.com>
2024-04-24 21:52:02 -0700

    Create template.s
    add template.s

---
+++ example folder/template.s
@@ -0,0 +1,27 @@
+   .global function_or_driver_name
+
+   .data
+
+   .text
+function_or_driver_name:
+   stp   x19, x20, [sp, #-16]!   // Push x19 and x20, then move SP down 16 bytes
+   stp   x21, x22, [sp, #-16]!   // Push x21 and x22, then move SP down 16 bytes
+   stp   x23, x24, [sp, #-16]!   // Push x23 and x24, then move SP down 16 bytes
+   stp   x25, x26, [sp, #-16]!   // Push x25 and x26, then move SP down 16 bytes
+   stp   x27, x28, [sp, #-16]!   // Push x27 and x28, then move SP down 16 bytes
+   stp   x29, x30, [sp, #-16]!   // Push x29 and x30, then move SP down 16 bytes
+
+
+
+
+
+   ldp   x29, x30, [sp], #16     // Pop x29 and x30, then move SP up 16 bytes
+   ldp   x27, x28, [sp], #16     // Pop x27 and x28, then move SP up 16 bytes  
+   ldp   x25, x26, [sp], #16     // Pop x25 and x26, then move SP up 16 bytes
+   ldp   x23, x24, [sp], #16     // Pop x23 and x24, then move SP up 16 bytes
+   ldp   x21, x22, [sp], #16     // Pop x21 and x22, then move SP up 16 bytes
+   ldp   x19, x20, [sp], #16     // Pop x19 and x20, then move SP up 16 bytes
+
+   RET                           // Return to caller
+
+   .end
