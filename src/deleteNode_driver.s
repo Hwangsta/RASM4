26edb338421a238e1dae8df87f7abc37180df3d4
Masonman101 <165975990+Masonman101@users.noreply.github.com>
2024-04-28 23:06:53 -0700

    Update deleteNode_driver.s

--- src/deleteNode_driver.s
+++ src/deleteNode_driver.s
@@ -11,14 +11,19 @@ deleteNode_driver:
    stp   x27, x28, [sp, #-16]!   // Push x27 and x28, then move SP down 16 bytes
    stp   x29, x30, [sp, #-16]!   // Push x29 and x30, then move SP down 16 bytes
 
-// x0 = headPtr, x1 = tailPtr, x2 = index number
-
+// x0 = headPtr, x1 = tailPtr, x2 = index number, x3 = numNodes, x4 = strBytes
+        mov     x21, x3                                         // save numNodes in x21
+        mov     x22, x4                                         // save strBytes in x22
+        mov     x23, x0                                         // save headPtr in x23
+        mov     x24, x1                                         // save tailPtr in x24
         traverse:
         // Additionally, we have to preserve the LR due to BL MALLOC
         mov     x19,x0
         mov     x20, #1                                         // initialize the counter to 1
 
-        sub     x2, x2, #1
+        cmp     x2, #1                                          // check if we are at first node
+   beq  first_index                                     // jump to first index if we are at first node
+     sub     x2, x2, #1
 
 traverse_loop:
    ldr     x19,[x19]
@@ -37,11 +42,29 @@ traverse_loop:
 previous_index_found:
         sub     x19,x19,#8
 
-                mov     x3,x19
+   mov     x26,x19
 
    add   x19,x19,#8                              // jump the x1 -> next field
    ldr     x20,[x19]                                                                             // dereference the next pointer
 
+        // delete one from numNodes
+        ldr     x25,[x21]                                                               // de-reference numNodes address to get node #
+        sub     x4, x25, #1                                                             // decrement node number
+        str     x4,[x21]
+
+        // subtract string's bytes and update strBytes
+        ldr     x0,[x20]
+        bl              String_length
+
+        add     x0, x0, #1                                                              // add null
+
+        ldr     x4, [x22]
+        sub     x0, x4, x0
+        str     x0,[x22]
+
+   mov  x3,x26
+        mov     x1,x24
+        mov     x0,x23                                                                  // reload headPtr into x0
         mov     x2, x20
         bl              deleteNode
 
@@ -56,4 +79,30 @@ traverse_exit:
 
    RET                           // Return to caller
 
-   .end
+first_index:
+        // delete one from numNodes
+        ldr     x25,[x21]                                                               // de-reference numNodes address to get node #
+        sub     x4, x25, #1                                                             // decrement node number
+        str     x4,[x21]
+
+        // subtract string's bytes and update strBytes
+        ldr     x0,[x23]
+        ldr     x0,[x0]
+        bl              String_length
+
+        add     x0, x0, #1                                                              // add null
+
+        ldr     x4, [x22]
+        sub     x0, x4, x0
+        str     x0,[x22]
+
+        mov     x3,#0x0                                                                         // set previ
+        mov     x0,x23
+        mov     x1,x24
+        ldr     x2,[x0]
+
+        bl              deleteNode
+
+        b               traverse_exit
+
+  .end
