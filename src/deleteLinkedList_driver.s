57ea81f267dcaa510d1c666d1bfd54f6ad545115
Masonman101 <165975990+Masonman101@users.noreply.github.com>
2024-05-01 18:44:40 -0700

    Create deleteLinkedList_driver.s

---
+++ src/deleteLinkedList_driver.s
@@ -0,0 +1,47 @@
+   .global deleteLinkedList_driver
+
+   .data
+
+   .text
+deleteLinkedList_driver:
+   stp   x19, x20, [sp, #-16]!   // Push x19 and x20, then move SP down 16 bytes
+   stp   x21, x22, [sp, #-16]!   // Push x21 and x22, then move SP down 16 bytes
+   stp   x23, x24, [sp, #-16]!   // Push x23 and x24, then move SP down 16 bytes
+   stp   x25, x26, [sp, #-16]!   // Push x25 and x26, then move SP down 16 bytes
+   stp   x27, x28, [sp, #-16]!   // Push x27 and x28, then move SP down 16 bytes
+   stp   x29, x30, [sp, #-16]!   // Push x29 and x30, then move SP down 16 bytes
+
+// x0 = headPtr
+
+   ldr     x19,[x0]
+freeNode_top:
+
+   cmp     x19,#0                                  // checking for empty list
+   beq     freeNode_exit
+
+   mov     x20,x19
+
+   add     x19,x19,#8                              // jump the x1 -> next field
+
+   ldr     x19,[x19]
+
+   ldr     x0,[x20]                                // double de-reference
+   bl      free
+
+   mov     x0,x20                          // move address
+   bl      free
+
+   b       freeNode_top
+
+freeNode_exit:
+
+   ldp   x29, x30, [sp], #16     // Pop x29 and x30, then move SP up 16 bytes
+   ldp   x27, x28, [sp], #16     // Pop x27 and x28, then move SP up 16 bytes
+   ldp   x25, x26, [sp], #16     // Pop x25 and x26, then move SP up 16 bytes
+   ldp   x23, x24, [sp], #16     // Pop x23 and x24, then move SP up 16 bytes
+   ldp   x21, x22, [sp], #16     // Pop x21 and x22, then move SP up 16 bytes
+   ldp   x19, x20, [sp], #16     // Pop x19 and x20, then move SP up 16 bytes
+
+   RET                           // Return to caller
+
+   .end
